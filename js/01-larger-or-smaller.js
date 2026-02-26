8fd317729040ee5dfd0cef910816f911283eb1f8
Meli <ms.melcastro@gmail.com>
2026-02-25 16:29:00 -0800

    adjusted javascript 1 &2

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -2,9 +2,9 @@ let num1 = parseInt(prompt("Enter the first number:"));
 let num2 = parseInt(prompt("Enter the second number:"));
 
 if (num1 === num2) {
-    alert("Both numbers are equal.");
+    console.log("Both numbers are equal.");
 } else if (num1 > num2) {
-    alert("The larger number is: " + num1);
+    console.log("The larger number is: " + num1);
 } else {
-    alert("The larger number is: " + num2);
+    console.log("The larger number is: " + num2);
 }
--- js/02-word-translator.js
+++ js/02-word-translator.js
@@ -1,14 +1,14 @@
 let language = prompt("Enter a language code (es, de, en, fr):");
 
 if (language === "fr") {
-    alert("Hello World translated in French is: Bonjour le monde");
+    console.log("Hello World translated in French is: Bonjour le monde");
 } 
 else if (language === "es") {
-    alert("Hello World translated in Spanish is: Hola Mundo");
+    console.log("Hello World translated in Spanish is: Hola Mundo");
 } 
 else if (language === "de") {
-    alert("Hello World translated in German is: Hallo Welt");
+    console.log("Hello World translated in German is: Hallo Welt");
 } 
 else {
-    alert("Hello World translated in English is: Hello World");
+    console.log("Hello World translated in English is: Hello World");
 }
